8a4e31cf6f6d9e8bc25d5dcf8476f647aff911fc
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-20 21:55:00 +0300

    renamed serializers.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,7 +1,9 @@
 import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+User = get_user_model()
 
 class Listing(models.Model):
     PROPERTY_TYPES = (
