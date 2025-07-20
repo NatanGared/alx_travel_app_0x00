c02468ce2002b6605f119b76b3c938fe48a6836a
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-20 21:49:43 +0300

    renamed serializers.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,9 +1,7 @@
 import uuid
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-User = get_user_model()
 
 class Listing(models.Model):
     PROPERTY_TYPES = (
--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
 from .models import Listing, Booking, Review
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
